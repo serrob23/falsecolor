18ae942f7bb8b11d3cbcb53c49d7a834cdb0c3f2
Robert Serafin <robert.b.serafin@gmail.com>
2019-11-20 14:19:39 -0800

    created new methods for sorting image array values based on threshold value, storing and saving returned data in a convenient way

--- FalseColor/Process.py
+++ FalseColor/Process.py
@@ -36,14 +36,112 @@ import cv2
 import numpy
 import h5py as hp
 from numba import cuda, njit
-import math
+import json
 
 
 @njit
-def sortImage(image,mask_val = 255):
-    s = []
+def sortImage(image, mask_val = 255):
+    """
+    Method for sorting image pixel values excluding a high value threshold. Used by getRGBStats to 
+    get a 1D array for pixel values less than saturated value.
+
+    Parameters
+    ----------
+
+    image : 2D numpy array
+        Image array 
+
+    mask_val : int
+        High threshold over which pixel values will be ignored
+
+
+    Returns
+    -------
+    pixel_set : 1D numpy array
+        Pixel values contained in image which are under the mask value.
+
+    """
+    pixel_set = []
+
+    #Ravel image and sort through item by item
     image_list = image.ravel()
     for item in image_list:
         if item < mask_val:
-            s.append(item)
-    return s
+            #append to pixel set if value is under threshold
+            pixel_set.append(item)
+    return numpy.asarray(pixel_set)
+
+def getRGBStats(image, mask_val = 255):
+    """
+    Method which returns dictionary containing useful image statistics from the input RGB image. 
+    Will be called from a save metadata method which will store data in a json.
+
+
+    Parameters
+    ----------
+
+    image : 3D numpy array
+        RGB image array 
+
+    mask_val : int
+        High threshold over which pixel values will be ignored
+
+    Returns
+    -------
+
+    image_stats : dict
+        Dictionary with 'R', 'G', 'B' keys which each have a dict as follows:
+
+            'data' : data called from sortImage method, below mask value
+
+            'median' : median of sorted image data
+
+            '90th' : 90th percentile of sorted image data
+
+            '10th' : 10th percentile of sorted image data
+
+    """
+    R = {'data' : sortImage(image[:,:,0], mask_val = mask_val)}
+    G = {'data' : sortImage(image[:,:,1], mask_val = mask_val)}
+    B = {'data' : sortImage(image[:,:,1], mask_val = mask_val)}
+    
+    R['median'] = numpy.median(R['data'])
+    G['median'] = numpy.median(G['data'])
+    B['median'] = numpy.median(B['data'])
+
+    R['90th'] = numpy.percentile(R['data'], 90)
+    R['10th'] = numpy.percentile(R['data'], 10)
+
+    G['90th'] = numpy.percentile(G['data'], 90)
+    G['10th'] = numpy.percentile(G['data'], 10)
+
+    B['90th'] = numpy.percentile(B['data'], 90)
+    B['10th'] = numpy.percentile(B['data'], 10)
+
+    image_stats = {'R' : R, 'G' : G, 'B' : B}
+
+    return image_stats
+
+def saveRGBStats(image_stats,folder,filename):
+    """
+    Parameters
+    ----------
+
+    image_stats : dict
+        dictionary of the same form as the one returned from getRGBStats
+
+    folder : str or path
+        string or pathlike object corresponding to the storage directory where the npz file 
+        will be saved
+
+    filename : str
+        name of file to be saved
+
+    Returns
+    -------
+
+    """
+
+    savepath = os.path.join(folder,filename)
+
+    numpy.savez(savepath, R = image_stats['R'], G = image_stats['G'], B = image_stats['B'])
