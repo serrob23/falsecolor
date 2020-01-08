4f7f03b62f7e2424fcabcb25bf3dec39e91616c1
Robert Serafin <serrob23@uw.edu>
2020-01-08 15:16:01 -0800

    updated setup.py

--- setup.py
+++ setup.py
@@ -31,9 +31,8 @@ setup(name='FalseColor',
     classifiers = [
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
-        'License :: GNU GPL v3.0',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7'
     ],
 
-                        )
+)
