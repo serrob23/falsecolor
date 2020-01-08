46ae573bfce3772f0ce181aadc868b9fa16959f1
Robert Serafin <robert.b.serafin@gmail.com>
2020-01-08 10:46:59 -0800

    added features for pip publication

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
 from distutils.core import setup
 
 setup(name='FalseColor',
-    version='0.1.1',
-    description='Methods for H&E pseudo coloring of grayscale images',
+    packages = ['FalseColor'],
+    version='0.1.5',
+    license = 'GNU Affero General Public License v3.0',
+    description='Methods for H&E pseudo coloring of grayscale fluorescent images',
     author='Robert Serafin',
+    author_email='serrob23@uw.edu',
+    url = 'https://github.com/serrob23/FalseColor',
+    download_url = '',
+    keywords = ['Image Analysis', 
+                            'Fluorescence Microscopy', 
+                            'Color Translation', 
+                            'Virtual Staining',
+                            'Histology'],
     install_requires =[
                         'opencv-python',
                         'numpy',
@@ -15,6 +25,13 @@ setup(name='FalseColor',
                         'h5py',
                         'functools',
                         ],
-    url = 'https://github.com/serrob23/falseColoring', 
-    author_email='serrob23@uw.edu',
-    packages=['FalseColor'])
+    classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: GNU Affero General Public License v3.0'
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7'
+
+    ],
+
+                        )
